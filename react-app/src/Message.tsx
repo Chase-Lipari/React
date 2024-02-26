0e431fd812b07b36a21d0ae721ae53885e38ec9c
[Chase <75291063+Chase-Lipari@users.noreply.github.com>
2024-02-26 18:36:40 -0500

    commit

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,8 @@
+
+
+function Message(){
+    const name = 'Chase';
+    return <h1>Hello {name}</h1>
+}
+
+export default Message;
---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,19 @@
+function ListGroup() {
+  let items = ["New york", "San Fran", "Tokyo", "London"];
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 ? <p>No item found</p> : null}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
